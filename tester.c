421e5505eed8a763ab63742960fc57b5719dcad6
Noah Gibbs <noah@ooyala.com>
2012-05-30 23:04:12 -0700

    Add one more deliver_with handler for testing

--- tester.c
+++ tester.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "hastur.h"
+
+/* Deliver_with handlers: */
 
 int print_message(const char *message, void *user_data) {
   printf("Hastur message:\n%s\n", message);
 
   return 0;
 }
+
+int copy_message(const char *message, void *copy_to) {
+  strcpy(copy_to, message);
+
+  return 0;
+}
+
+/* Main */
 
 int main(int argc, char **argv) {
   hastur_deliver_with(print_message, NULL);
