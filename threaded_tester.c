90ff8714d79cf5d37ccba48bc4c99ff78347b651
Noah Gibbs <noah@ooyala.com>
2012-06-05 16:39:08 -0700

    Have threaded_tester check (and fail) the registration message.  Need to figure out how to do thread_ids that aren't main.

--- threaded_tester.c
+++ threaded_tester.c
@@ -5,13 +5,25 @@
 #include "test_helper.h"
 #include "hastur.h"
 
+time_t time_plus_n(void *user_data) {
+  int n = (long int)user_data;
+
+  return time(0) + n;
+}
+
 int main(int argc, char **argv) {
   hastur_deliver_with(copy_to_buf, NULL);
   hastur_timestamp_with(use_fake_timestamp, NULL);
   hastur_set_app_name("thread_tester");
 
-  /* No process registration will be sent over UDP because we set deliver_with */
+  /* hastur_bg_time_with(time_plus_n, (void*)0); */
+
+  /* Process registration will be captured by copy_to_buf */
   hastur_start();
+  assert_message_equal("{\"type\":\"reg_process\","
+		       "\"data\":{},"
+		       "\"timestamp\":" FAKE_NOW_TIME_STRING ","
+		       "\"labels\":{\"app\":\"thread_tester\",\"pid\":-PID-,\"tid\":\"main\"}}", NULL);
 
   hastur_counter("my.counter", 7);
   assert_message_equal("{\"type\":\"counter\","
